cfc3ae644ac07b4ebd5f028f84dcc77250c2cb8b
Allan Chávez <allancss2009@hotmail.com>
2023-08-17 20:25:16 -0600

    feat: Implemented a searchbar with useState

--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "../assets/styles/styles.module.css";
 
@@ -10,24 +10,41 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 function NoteList({ notes, deleteNote, watchNoteFunction }) {
-  const bgNotesColors = notes?.map((note) => {
-    return note.bg_color
+  const [filteredNotes, setFilteredNotes] = useState([]);
+  const bgNotesColors = filteredNotes?.map((note) => {
+    return note.bg_color;
   });
 
   useEffect(() => {
-    console.log("bgNotesColors")
-  })
+    console.log("bgNotesColors");
+    setFilteredNotes(notes)
+  }, []);
 
-    // const watchNoteFunction = (note) =>{
-  //   console.log(note);
-  //   setNoteToOpen(note);
+  const [searchInput,setSearchInputValue] = useState('');
 
-  // }
-  const handleDeleteNote = (note) => {
-    // console.log(note);
-    // let newNotes = notes.filter((n) => n.id !== note.id);
-    // setNotes(newNotes);
-    // console.log("Deleting a note");
+  const handleSubmit = (event) => {
+    event.preventDefault(); // Prevent the default form submission
+    console.log(filteredNotes)
+    const filtered = notes.filter((note) =>{
+      console.log(note.title.toLowerCase().includes(searchInput.toLowerCase()))
+      console.log(searchInput)
+      if(note.title.toLowerCase().includes(searchInput.toLowerCase())){
+        console.log("Oh yeah, one match")
+        return note
+      }
+    }
+
+    );
+
+    setFilteredNotes(filtered);
+    console.log(notes)
+    console.log(filtered)
+    // Perform your actions here
+    console.log('Form submitted with value:', searchInput);
+  };
+
+  const handleInputChange = (event) => {
+   setSearchInputValue(event.target.value);
   };
 
 
@@ -37,12 +54,52 @@ function NoteList({ notes, deleteNote, watchNoteFunction }) {
         <FontAwesomeIcon icon={faStickyNote} className="mr-2" />
         MY NOTEBOOK
       </h1>
+
+      <div className="my-4">
+        <form  onSubmit={handleSubmit}>
+          <div className="relative">
+            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+              <svg
+                className="w-4 h-4 text-gray-500"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  stroke="currentColor"
+                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                />
+              </svg>
+            </div>
+            <input
+              type="search"
+              id="notes-search"
+              value={searchInput}
+              onChange={handleInputChange}
+              className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-transparent focus:border-transparent "
+              placeholder="Search among your notes..."
+            />
+            <button
+              type="submit"
+              className="text-white absolute right-2.5 bottom-2.5 bg-yellow-400 hover:bg-yellow-500  font-medium rounded-lg text-sm px-4 py-2"
+            >
+              Search
+            </button>
+          </div>
+        </form>
+      </div>
+
       <div className="grid grid-flow-row auto-rows-max sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 2xl:grid-cols-6 gap-4 ">
-        {notes?.map((note, index) => (
+        {filteredNotes?.map((note, index) => (
           <div
             key={index}
-            className={ "w-48 h-48 relative rounded-lg shadow-md cursor-pointer " + (bgNotesColors[index] !== undefined? bgNotesColors[index] :"bg-gray-200") }
-           
+            className={
+              "w-48 h-48 relative rounded-lg shadow-md cursor-pointer " +
+              (bgNotesColors[index] !== undefined
+                ? bgNotesColors[index]
+                : "bg-gray-200")
+            }
           >
             <h3
               className="text-lg font-bold m-2 flex-grow overflow-hidden whitespace-nowrap text-overflow-ellipsis"
@@ -53,7 +110,10 @@ function NoteList({ notes, deleteNote, watchNoteFunction }) {
 
             <p
               title={note.description}
-              className={`${styles["truncate-overflow"]} text-clip text-justify px-4 py-2 `+(note.text_color || "text_black")}
+              className={
+                `${styles["truncate-overflow"]} text-clip text-justify px-4 py-2 ` +
+                (note.text_color || "text_black")
+              }
             >
               {note.description}
             </p>
@@ -87,16 +147,16 @@ function NoteList({ notes, deleteNote, watchNoteFunction }) {
           </div>
         ))}
       </div>
-      {/* <div class="w-16 h-16 bg-gray-500"></div>
-    <div class="w-16 h-16 bg-gray-300"></div>
-    <div class="w-16 h-16 bg-gray-500"></div>
-    <div class="w-16 h-16 bg-gray-300"></div>
-    <div class="w-16 h-16 bg-gray-500"></div>
-    <div class="w-16 h-16 bg-gray-300"></div>
-    <div class="w-16 h-16 bg-gray-500"></div>
-    <div class="w-16 h-16 bg-gray-300"></div>
-    <div class="w-16 h-16 bg-gray-500"></div> */}
-      {/* {notes?.map((note, index) => (
+      {/* <div className="w-16 h-16 bg-gray-500"></div>
+    <div className="w-16 h-16 bg-gray-300"></div>
+    <div className="w-16 h-16 bg-gray-500"></div>
+    <div className="w-16 h-16 bg-gray-300"></div>
+    <div className="w-16 h-16 bg-gray-500"></div>
+    <div className="w-16 h-16 bg-gray-300"></div>
+    <div className="w-16 h-16 bg-gray-500"></div>
+    <div className="w-16 h-16 bg-gray-300"></div>
+    <div className="w-16 h-16 bg-gray-500"></div> */}
+      {/* {filteredNotes?.map((note, index) => (
         <div key={index} className="bg-white p-4 rounded-lg shadow-md mb-4 ">
           <div className="flex items-center">
             <div className="w-4/5 text-center">
